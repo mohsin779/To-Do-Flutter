35bed3306a3f21e9cc943d4ce3ea4b4c52a33894
mohsin779 <mianmohsin779@gmail.com>
2023-01-25 17:33:28 +0500

    added a floating action button to delete a post

--- lib/screens/post_detail.dart
+++ lib/screens/post_detail.dart
@@ -5,12 +5,14 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../provider/posts.dart';
+import '../screens/add_post.dart';
 
 class PostDetail extends StatelessWidget {
   static const routeName = '/post-detail';
 
   @override
   Widget build(BuildContext context) {
+    final scaffoldMessenger = ScaffoldMessenger.of(context);
     final postId = ModalRoute.of(context).settings.arguments as String;
     final loadedPost =
         Provider.of<Posts>(context, listen: false).findById(postId);
@@ -18,6 +20,15 @@ class PostDetail extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedPost.title),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(AddPost.routeName,
+                  arguments: {'postId': loadedPost.id, 'title': 'Edit Post'});
+            },
+            icon: const Icon(Icons.edit),
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(children: <Widget>[
@@ -41,6 +52,26 @@ class PostDetail extends StatelessWidget {
             ),
           )
         ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.delete),
+        onPressed: () async {
+          try {
+            await Provider.of<Posts>(context, listen: false)
+                .deletePost(loadedPost.id);
+            print('after');
+            Navigator.of(context).pop();
+          } catch (error) {
+            scaffoldMessenger.showSnackBar(
+              SnackBar(
+                content: Text(
+                  'Deleting post failed',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            );
+          }
+        },
       ),
     );
   }
