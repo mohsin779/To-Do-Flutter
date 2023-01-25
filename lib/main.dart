29ca825fef0a58e9d5660a8cf2f748da8440e059
mohsin779 <mianmohsin779@gmail.com>
2023-01-25 10:13:57 +0500

    initial setup

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ToDo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("To Do's"),
+      ),
+      body: Center(
+        child: Text('Let\'s create a ToDo!'),
+      ),
+    );
+  }
+}
